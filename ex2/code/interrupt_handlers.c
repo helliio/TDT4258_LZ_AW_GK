eccf14b8be01f42bbd8486562a8783b901a17a3f
Geir Kulia <geir@kulia.no>
2015-03-06 04:58:03 +0100

    Structured folders and add drum sound when button is pushed

--- ex2/code/interrupt_handlers.c
+++ ex2/code/interrupt_handlers.c
@@ -2,29 +2,41 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
-int sound = 100;
-int sound_value = 100;
+#include "values.h"
+#include "drum_sound.h"
+
+int sound = 0;
+int sound_value = 0;
 void interrupt_handler();
 /* TIMER1 interrupt handler */
 
+
+
+int i = 0;
+int button_pushed = -1;
+
+int * sound_sample_left;
+
 void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
 {  
-    if (sound < 1000){
-        sound += sound_value;
-    }
-    if (sound >= 1000){
-        sound = sound_value;
-    }  
-    *TIMER1_IFC = 1;    
-    *DAC0_CH0DATA = sound;
-    *DAC0_CH1DATA = sound;
-    
-    
 
-    /*
-    TODO feed new samples to the DAC
-    remember to clear the pending interrupt by writing 1 to TIMER1_IFC
-    */  
+    *TIMER1_IFC = 1; 
+
+    if (button_pushed == 7)
+    {
+       play_drum_sound(); 
+    }
+    else
+    {
+        sound = 0;
+    }
+
+    display_power_bar();
+}
+
+void display_power_bar()
+{
+     * GPIO_PA_DOUT = 0xff00 >> (sound * 8 / 0xfff);
 }
 
 /* GPIO even pin interrupt handler */
@@ -41,17 +53,31 @@ void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 
 void interrupt_handler(){
 
-    *GPIO_IFC = 0xff;
+    * GPIO_IFC = 0xff;
+
     uint32_t input = *GPIO_PC_DIN;
     uint32_t sw7 = input & 0x40;
     uint32_t sw5 = input & 0x10;
-    if (sw7 != 0 && sound_value < 200){
-        sound_value += 10;
+    if (sw7 == 0){
+        button_pushed = 7; 
     }
-    if (sw5 != 0 && sound_value > 100){
-        sound_value -= 10;
-    }
-    
+
+    // * GPIO_PA_DOUT =  * GPIO_PA_DOUT << 8;
 }
 
+void play_drum_sound()
+{
+    sound = drum_sound[i];
+    i++;
 
+    *DAC0_CH0DATA = sound;
+    *DAC0_CH1DATA = sound;
+
+
+    if (i >= lenght_of_drum_sound)
+    {
+        button_pushed = -1;
+        i = 0;
+    }
+
+}
