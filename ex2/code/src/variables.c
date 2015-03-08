a3e8511002718cb8bb25d1655239b0fa1b5ce351
Geir Kulia <geir@kulia.no>
2015-03-09 00:40:05 +0100

    Ex2: Add file with global variables

---
+++ ex2/code/src/variables.c
@@ -0,0 +1,7 @@
+#include "defines.h"
+// Variables used in more than one file
+
+int     base_sample_period  = 2500;
+int     sample_period       = 2500;
+bool    trigger_sound       = false;
+int 	piano_position 		= 0;
