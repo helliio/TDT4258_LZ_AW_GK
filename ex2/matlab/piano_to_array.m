4c904fbea739724e8e9ffe21db1cc4310b6019bc
Kulia <geir@kulia.no>
2015-03-09 02:21:03 +0100

    Ex2: Clean code.

--- ex2/matlab/piano_to_array.m
+++ ex2/matlab/piano_to_array.m
@@ -1,13 +1,9 @@
 %Fs = 44100;
 [y,Fs] = audioread('piano.mp3');
-
 y = y(:, 1); % Make signal mono
+
 y = y(y ~= 0); % Remove all zeros
-y = y(1:8:length(y)/5); % decrese sempling frequency and length of signal.
-
-sound (y, 43750/8)
-
-
+y = y(1:8:length(y)/5); % decrese sampling frequency and reverberation
 y = round((y + 1) .* 2^11);
 y = y(y ~= 2048); % 0x800 repeats itself a lot in the beginning
 
