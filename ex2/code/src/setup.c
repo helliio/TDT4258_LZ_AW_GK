2dc4c5eb41021eadb41047f6aff9e72760d64330
helliio <helliio@users.noreply.github.com>
2015-03-09 01:13:33 +0100

    add setup gpio comment

--- ex2/code/src/setup.c
+++ ex2/code/src/setup.c
@@ -25,11 +25,11 @@ void setupGPIO()
     *GPIO_PA_CTRL       =   2;  /* set high drive strength */
     *GPIO_PA_MODEH      =   0x55555555; /* set pins A8-15 as output */
     *GPIO_PA_DOUT       =   0x0000; /* turn on LEDs D1-D8 (LEDs are active low) */
-    *GPIO_PC_MODEL      =   0x33333333;
-    *GPIO_PC_DOUT       =   0xff;
-    *GPIO_EXTIPSELL     =   0x22222222;
-    *GPIO_EXTIFALL      =   0xff;
-    *GPIO_IEN           =   0xff;
+    *GPIO_PC_MODEL      =   0x33333333; /* Set pins 0-7 */
+    *GPIO_PC_DOUT       =   0xff; /* Enable internal pull-up */
+    *GPIO_EXTIPSELL     =   0x22222222; /* Write 0x22222222 to register */
+    *GPIO_EXTIFALL      =   0xff; /* Set interrupt on 1->0 transition */
+    *GPIO_IEN           =   0xff; /* Enable interrupt generation */
 }
 
 /* function to setup the timer */
